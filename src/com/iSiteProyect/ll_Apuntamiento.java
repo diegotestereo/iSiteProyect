070756c167e1e5438ea64d8091a0e62b9e5cd346
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2014-10-28 16:35:15 -0300

    se agrego pantalla gps
    apuntamiento calculo

---
+++ src/com/iSiteProyect/ll_Apuntamiento.java
@@ -0,0 +1,21 @@
+package com.iSiteProyect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ll_Apuntamiento extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ll_apuntamiento);
+		LevantarXML();
+		
+	}
+
+	private void LevantarXML() {
+		
+		
+	}
+
+}
